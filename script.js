199b1fe15a5bfa5ed3b5866180efed62da18b9a4
Wilfred Osozi <osoziw@gmail.com>
2025-01-26 22:11:23 +0300

    updated the camelcase

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// Attach Event Listeners:
 document.addEventListener ('DOMContentLoaded', ()=>{
 
+// Select DOM Elements:
 const addButton = document.getElementById ('add-task-btn')
 const taskInput = document.getElementById ('task-input')
 const taskList = document.getElementById ('task-list')
 
 
+// Create the addTask Function:
 function addTask (){
    const taskText = taskInput.value.trim ()
 
@@ -12,20 +15,20 @@ function addTask (){
     alert('please enter a task')
     return;
    }
-
-const taskitem = document.createElement ('li')
-taskitem.textContent = taskText
+// Task Creation and Removal:
+const taskItem = document.createElement ('li')
+taskItem.textContent = taskText
 taskList.appendChild(taskitem)
 
-const removebt= document.createElement('button')
-removebt.textContent = "Remove"
-removebt.className = "remove-btn"
-removebt.addEventListener ('click', () => {
-    taskList.removeChild (taskitem)
+const removeBt= document.createElement('button')
+removeBt.textContent = "Remove"
+removeBt.className = "remove-btn"
+removeBt.addEventListener ('click', () => {
+    taskList.removeChild (taskItem)
 })
 
 
-taskitem.appendChild(removebt)
+taskItem.appendChild(removebt)
 
 
 taskList.appendChild(taskitem)
@@ -37,20 +40,9 @@ taskInput.value = ""
 
 addButton.addEventListener ('click', addTask);
 taskInput.addEventListener ('keypress', (event) =>{
-if (event.key===Enter){
+if (event.key==='Enter'){
     addTask ();
 }
 })
-
-
-
-
-
-
-
-
-
-
-
 
 })
